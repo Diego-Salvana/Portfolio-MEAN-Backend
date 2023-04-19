69f1036f67a47c8e0f19e75ed42f121a6f08db49
Diego Salvañá <diegofsalvana@gmail.com>
2023-04-19 17:46:32 -0300

    no dist

--- dist/utils/error.handle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttpError = void 0;
-const handleHttpError = (res, error, errorRaw) => {
-    console.log('Controller error', errorRaw);
-    res.status(500).send(error);
-};
-exports.handleHttpError = handleHttpError;
